e5691486b3404103224617001d78f4221224d63a
Ελισάβετ Λιάνα <32645328+elisavetliana@users.noreply.github.com>
2018-10-17 14:03:40 +0300

    Update Functions.js

--- scripts/Functions.js
+++ scripts/Functions.js
@@ -21,3 +21,62 @@ function fireBullet() {
 		}
 	}
 }
+
+function launchEnemies1() {
+	var enemy = enemies1.getFirstExists(false);
+	if (enemy) {
+		enemy.reset( game.width, game.rnd.integerInRange ( 20, 580));
+		enemy.body.velocity.y = game.rnd.integerInRange(-200, 200);
+		enemy.body.velocity.x = -300;
+		enemy.body.drag.y = 100;
+		console.log("Enemy Spawned");
+
+		enemy.trail.start(false, 800, 1);
+
+		enemy.update = function(){
+			enemy.angle = 270 - game.math.radToDeg(Math.atan2(enemy.body.velocity.x, enemy.body.velocity.y));
+
+			enemy.trail.x = enemy.x;
+			enemy.trail.y = enemy.y;
+
+			//Kill enemies off screen
+			if (enemy.x < -200)
+				enemy.kill();
+        }
+	}
+
+	//  Send another enemy soon
+	game.time.events.add(game.rnd.integerInRange(300, 3000), launchEnemies1);
+}
+
+function addEnemyEmitterTrail(enemy) {
+	var enemyTrail = game.add.emitter(enemy.x, player.y - 10, 100);
+	enemyTrail.width = 10;
+	enemyTrail.makeParticles('explosion', [1,2,3,4,5]);
+	enemyTrail.setXSpeed(20, -20);
+	enemyTrail.setRotation(50,-50);
+	enemyTrail.setAlpha(0.4, 0, 800);
+	enemyTrail.setScale(0.01, 0.1, 0.01, 0.1, 1000, Phaser.Easing.Quintic.Out);
+	enemy.trail = enemyTrail;
+}
+
+function shipCollide(player, enemy) {
+	var explosion = explosions.getFirstExists(false);
+	explosion.reset(enemy.body.x + enemy.body.halfWidth, enemy.body.y + enemy.body.halfHeight);
+	explosion.body.velocity.y = enemy.body.velocity.y;
+	explosion.alpha = 0.7;
+	explosion.play('explosion', 30, false, true);
+	explosionSound.play('',0,0.5,false);
+	enemy.kill();
+}
+
+function hitEnemy(enemy, bullet) {
+	var explosion = explosions.getFirstExists(false);
+	explosion.reset(bullet.body.x + bullet.body.halfWidth, bullet.body.y + bullet.body.halfHeight);
+	explosion.body.velocity.y = enemy.body.velocity.y;
+	explosion.alpha = 0.7;
+	explosion.play('explosion', 30, false, true);
+	explosionSound.play('',0,0.5,false);
+	enemy.kill();
+	bullet.kill()
+}
