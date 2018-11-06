301bb583c7f57b11c57fab01e344dda0a061972c
Ελισάβετ Λιάνα <32645328+elisavetliana@users.noreply.github.com>
2018-11-06 18:14:53 +0200

    Update Preload.js

--- scripts/Preload.js
+++ scripts/Preload.js
@@ -25,7 +25,7 @@ var PreloadState = {
 		this.load.image('title', 'assets/title.png');
 		this.load.image('lvl1', 'assets/levels/level1.png');
 		this.load.image('lvl2', 'assets/levels/level2.png');
-		this.load.image('enemy1', 'assets/enemies/newEnemy.png');
+		this.load.image('enemy1', 'assets/enemies/enemy2.png');
 		this.load.image('enemy2', 'assets/enemies/enemy3.png');
 		this.load.spritesheet('explosion', 'assets/explode.png', 128, 128);
 		this.load.bitmapFont('spacefont', 'assets/spacefont/spacefont.png', 'assets/spacefont/spacefont.xml');
